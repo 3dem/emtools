0dbf2db90b5165b0868eccbee22037e3c311101e
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2022-03-28 14:55:21 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,25 @@
+=======
+emtools
+=======
+
+Basic utilities for CryoEM data manipulation. It is available as a Python module (https://pypi.org/project/emtools).
+
+How to cite
+-----------
+
+Please cite the code repository DOI: xxx
+
+Authors
+-------
+
+ * Jose Miguel de la Rosa-Trevín, Department of Biochemistry and Biophysics, Science for Life Laboratory, Stockholm University, Stockholm, Sweden
+
+ 
+Testing
+-------
+
+``python -m unittest discover emtools/tests``
+
+Examples
+--------
+
---
+++ setup.py
@@ -0,0 +1,66 @@
+# **************************************************************************
+# *
+# * Authors:  J. M. de la Rosa Trevin (delarosatrevin@gmail.com)
+# * Authors:  Grigory Sharov (sharov.grigory@gmail.com)
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'delarosatrevin@gmail.com'
+# *
+# **************************************************************************
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='emtable',  # Required
+    version='0.0.9',  # Required
+    description='Simple module to deal with EM tabular data (aka metadata)',  # Required
+    long_description=long_description,  # Optional
+    url='https://github.com/delarosatrevin/emtable',  # Optional
+    author='J.M. De la Rosa Trevin, Grigory Sharov',  # Optional
+    author_email='delarosatrevin@gmail.com, gsharov@mrc-lmb.cam.ac.uk',  # Optional
+    classifiers=[  # Optional
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='electron-microscopy cryo-em structural-biology image-processing',  # Optional
+    packages=find_packages(),
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/delarosatrevin/emtable/issues',
+        'Source': 'https://github.com/delarosatrevin/emtable',
+    },
+)
