5559c86caa08b5a8467e5929af5316be7cdaea1a
Jose M. de la Rosa <delarosatrevin@gmail.com>
2023-01-11 11:09:54 -0600

    Removed session subpackage and added headers in utils

--- emtools/utils/pipeline.py
+++ emtools/utils/pipeline.py
@@ -14,7 +14,6 @@
 # *
 # **************************************************************************
 
-
 from collections import OrderedDict
 import threading
 
--- emtools/utils/process.py
+++ emtools/utils/process.py
@@ -13,6 +13,7 @@
 # * GNU General Public License for more details.
 # *
 # **************************************************************************
+
 import os
 import subprocess
 
